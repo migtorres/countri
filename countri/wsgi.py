3968ea2e03840bda18869d6a10dd1729ebb01e45
Miguel Torres <migtorres@gmail.com>
2016-02-19 23:45:57 +0000

    first commit

---
+++ countri/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for countri project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "countri.settings")
+
+application = get_wsgi_application()
